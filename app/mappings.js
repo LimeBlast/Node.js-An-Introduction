30484f1d544604a98d93f53351053e59ff8deae7
Daniel Hollands <daniel@limeblast.co.uk>
2014-04-04 21:05:12 +0100

    Changed mappings into a self-contained API.

--- app/mappings.js
+++ app/mappings.js
@@ -1,11 +1,23 @@
 'use strict';
 
-var mappings = {
+var data = {
     d: 'http://www.danielhollands.co.uk/',
     l: 'http://www.limeblast.co.uk/',
     g: 'http://www.google.com/',
     f: 'http://www.facebook.com/',
     t: 'http://www.twitter.com/'
+};
+
+var mappings = {
+	get: function(url) {
+		var alias = url.substring(1);
+
+		if (! data[alias]) {
+			return undefined;
+		}
+
+		return data[alias];
+	}
 };
 
 module.exports = mappings;
