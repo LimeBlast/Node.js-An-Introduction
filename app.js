8e7c062064b68f45e58010abe6a7e2675e30ef32
Daniel Hollands <daniel@limeblast.co.uk>
2014-04-04 20:33:31 +0100

    Changed content-type to text/html

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    })
 	res.write('Hello world!');
 	res.end();
 });
