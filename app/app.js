0c2bda72b5379127ebcba130aedca83d5880be63
Daniel Hollands <daniel@limeblast.co.uk>
2014-04-07 19:26:12 +0100

    Added express routing

--- app/app.js
+++ app/app.js
@@ -10,10 +10,9 @@ var logger = require('./logger'),
 var app = express();
 
 app.use(logger('redirector'));
-app.use(logger('redirector'));
 
-app.use(function(req, res) {
-	mappings.get(req.url, function(err, mapping) {
+app.get('/:alias', function(req, res) {
+	mappings.get(req.params.alias, function(err, mapping) {
 
 		if (err) {
 			res.writeHead(404);
